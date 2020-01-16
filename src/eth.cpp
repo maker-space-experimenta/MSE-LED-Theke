d31be6ca6798d8ce1df38774a4a7c2675a90ea3d
LeoDJ <leandro.spaeth@gmail.com>
2020-01-16 15:38:21 +0100

    restructure code to cpp files

---
+++ src/eth.cpp
@@ -0,0 +1,48 @@
+#include "eth.h"
+
+uint8_t mac[6];
+char macStr[13];
+EthernetClient net;
+
+void generateMAC(uint8_t *macArray) {
+
+    uint32_t uid0 = HAL_GetUIDw0();
+    uint32_t uid1 = HAL_GetUIDw1();
+    macArray[0] = 0x42;
+    macArray[1] = (uid0 >> 0) & 0xFF;
+    macArray[2] = (uid0 >> 8) & 0xFF;
+    macArray[3] = (uid0 >> 16) & 0xFF;
+    macArray[4] = (uid0 >> 24) & 0xFF;
+    macArray[5] = (uid1 >> 0) & 0xFF;
+
+    snprintf(macStr, 13, "%02X%02X%02X%02X%02X%02X", macArray[0], macArray[1], macArray[2], macArray[3], macArray[4], macArray[5]);
+}
+
+void initEthernet() {
+    SPI.setMOSI(PB15);
+    SPI.setMISO(PB14);
+    SPI.setSCLK(PB13);
+
+    Ethernet.init(PB12);
+
+    generateMAC(mac);
+
+    DEBUG.println("Initialize Ethernet with DHCP:");
+    if (Ethernet.begin(mac) == 0) {
+        DEBUG.println("Failed to configure Ethernet using DHCP");
+        if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+            DEBUG.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+            delay(5000);
+            HAL_NVIC_SystemReset();
+        } else if (Ethernet.linkStatus() == LinkOFF) {
+            DEBUG.println("Ethernet cable is not connected.");
+        }
+        // no point in carrying on, so do nothing forevermore:
+        while (true) {
+            delay(1);
+        }
+    }
+    // print your local IP address:
+    DEBUG.print("My IP address: ");
+    DEBUG.println(Ethernet.localIP());
+}
--- src/leds.h
+++ src/leds.h
@@ -1,6 +1,5 @@
-#include <Adafruit_NeoPixel.h>
-
-Adafruit_NeoPixel leds = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_BRG + NEO_KHZ800);
+#pragma once
+#include <Arduino.h>
 
 typedef struct {
     bool state;
@@ -9,88 +8,10 @@ typedef struct {
     String effect;
 } ledState_t;
 
-ledState_t curLedState = {
-    INIT_STATE,
-    INIT_BRIGHTNESS,
-    INIT_COLOR,
-    "static"
-};
-
-
-
-
-void applyLedState() {
-    leds.setBrightness(curLedState.brightness);
-    if(curLedState.effect == "static") {
-        leds.fill(curLedState.color);
-        leds.show();
-    }
-}
-
-void setLedState(bool state) {
-    curLedState.state = state;
-}
-
-void setLedBrightness(uint8_t brightness) {
-    curLedState.brightness = brightness;
-    leds.setBrightness(brightness);
-}
-
-void setLedColor(uint32_t color) {
-    curLedState.color = color;
-}
-
-void setLedEffect(String effect) {
-    curLedState.effect = effect;
-}
-
-void setLedState(ledState_t state) {
-    curLedState.state = state.state;
-    curLedState.brightness = state.brightness;
-    curLedState.color = state.color;
-    curLedState.effect = state.effect;
-
-    applyLedState();
-}
-
-ledState_t getLedState() {
-    return curLedState;
-}
-
-void initLeds() {
-    leds.begin();
-    applyLedState();
-}
-
-uint32_t lastLedUpdate = 0;
-uint16_t animationStep = 0;
-
-void loopLeds() {
-    if(millis() - lastLedUpdate > ANIMATION_SPEED) {
-        lastLedUpdate = millis();
-        // simple rainbow fade animation
-
-        if(!curLedState.state) {
-            leds.fill(0);
-            leds.show();
-        }
-        else {
-            if(curLedState.effect == "static") {
-                leds.fill(curLedState.color);
-                leds.show();
-            } else {
-                if(curLedState.effect = "rainbow") {
-                    uint32_t now = millis();
-                    for(uint16_t i = 0; i < leds.numPixels(); i++) {
-                        uint16_t timeOffset = (65535UL / 5000) * (now % 5000);
-                        uint16_t ledOffset = (65535UL * i) / (leds.numPixels());
-                        uint16_t hue = (timeOffset + ledOffset) % 65536UL;
-                        leds.setPixelColor(i, leds.ColorHSV(hue));
-                    }
-                    leds.show();
-                }
-                animationStep++;
-            }
-        }
-    }
-}
+void initLeds();
+void loopLeds();
+void setLedState(bool state);
+void setLedBrightness(uint8_t brightness);
+void setLedColor(uint32_t color);
+void setLedEffect(String effect);
+ledState_t getLedState();
